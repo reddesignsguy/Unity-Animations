6062b7399d1b251c9a0fe338ff8d43bed469bbbf
Albany Patriawan <49921782+reddesignsguy@users.noreply.github.com>
2024-11-26 15:12:13 -0800

    strafing and refactor

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -9,15 +10,22 @@ public class Player : MonoBehaviour
     private Animator animator;
 
     private float speed = 0f;
+    private float movementAngle = 90.0f;
+
+    private float targetSpeed = 0f;
+    private float targetMovementAngle = 90.0f;
     public float walkSpeed = 5f;
     public float runSpeed = 15f;
     public float acceleration = 0.5f;
     public float decceleration = 1f;
-    public float strafeRunAngle = 45.0f;
-    public float strafeAngle = 90.0f;
+    public float angularAcceleration = 600f;// degrees
+    public float angularDeccelleration = 300f; // used when "relaxing" (i.e: not pressing anything)
+    public float diagonal_strafeAngle = 45.0f;
+    public float horizontal_strafeAngle = 90.0f;
 
-    private RunState runState = RunState.Idle;
-    private StrafeState strafeState = StrafeState.None;
+    //private RunState runState = RunState.Idle;
+    //private HorizontalState strafeState = HorizontalState.None;
+    //private VerticalState verticalState = VerticalState.None;
 
     private void Awake()
     {
@@ -25,103 +33,150 @@ public class Player : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-    private enum RunState
-    {
-        Idle,
-        Walking,
-        Running
-    }
-
-    private enum StrafeState
-    {
-        None,
-        Right,
-        Left
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        // Check for directional inputs
+        bool forwardPressed = Input.GetKey(KeyCode.W);
+        bool backwardPressed = Input.GetKey(KeyCode.S);
+        bool leftPressed = Input.GetKey(KeyCode.A);
+        bool rightPressed = Input.GetKey(KeyCode.D);
+        bool sprintPressed = Input.GetKey(KeyCode.LeftShift);
+
+        // Get target speed
+        if (!forwardPressed && (rightPressed || leftPressed))
         {
-            if (Input.GetKey(KeyCode.LeftShift))
+            targetSpeed = walkSpeed;
+        }
+        else if (forwardPressed)
+        {
+            if (sprintPressed)
             {
-                runState = RunState.Running;
+                targetSpeed = runSpeed;
             }
             else
             {
-                runState = RunState.Walking;
+                targetSpeed = walkSpeed;
             }
         }
         else
         {
-            runState = RunState.Idle;
+            targetSpeed = 0f;
         }
-    }
 
-    void FixedUpdate()
-    {
-        switch(runState)
+        if (forwardPressed)
         {
-            case RunState.Walking:
-                Walking();
-                break;
-            case RunState.Running:
-                Running();
-                break;
-            case RunState.Idle:
-                Idle();
-                break;
+            if (leftPressed)
+            {
+                targetMovementAngle = 135f;
+            }
+            else if (rightPressed)
+            {
+                targetMovementAngle = 45f;
+            }
+            else
+            {
+                targetMovementAngle = 90f;
+            }
         }
-    }
-
-    private void Idle()
-    {
-        // Decellerate
-        speed -= decceleration * Time.deltaTime;
-        speed = Mathf.Max(speed, 0f);
-
-        Move(Vector3.forward);
-    }
-
-    private void Running()
-    {
-        // Aceelerate
-        speed += acceleration * Time.deltaTime;
-        speed = Mathf.Min(speed, runSpeed);
-
-        Move(Vector3.forward);
-    }
-
-    private void Walking()
-    {
-        // Accelerate
-        if (speed < walkSpeed)
+        else if (backwardPressed)
         {
-            speed += acceleration * Time.deltaTime;
-            speed = Mathf.Min(speed, walkSpeed);
+            //if (leftPressed)
+            //{
+            //    targetMovementAngle = 225f;
+            //}
+            //else if (rightPressed)
+            //{
+            //    targetMovementAngle = 315;
+            //}
+            //else
+            //{
+            //    targetMovementAngle = 270f;
+            //}
         }
-
-        // Decellerate
         else
         {
-            speed -= decceleration * Time.deltaTime;
-            speed = Mathf.Max(speed, walkSpeed);
+            if (leftPressed)
+            {
+                targetMovementAngle = 180f;
+            }
+            else if (rightPressed)
+            {
+                targetMovementAngle = 0f;
+            }
+            else
+            {
+                // Nothing pressed, do nothing
+            }
         }
-
-        if (strafeState == StrafeState.Left)
-        {
-
-        }
-
-        Move(Vector3.forward);
     }
 
-    private void Move(Vector3 direction)
+   void FixedUpdate()
     {
-        Vector3 movementVector = direction * speed;
+        UpdateSpeed();
+        UpdateMovementAngle();
+        Move();
+        UpdateAnimation();
+    }
+
+    private Vector3 GetMovementUnitVector(float angle)
+    {
+        Quaternion yawRotation = Quaternion.Euler(0, -angle, 0);
+        Vector3 rotatedVector = yawRotation * Vector3.right;
+        //print(rotatedVector);
+        return rotatedVector;
+    }
+
+    private void Move()
+    {
+        bool tryingToStop = targetSpeed == 0f;
+        float finalSpeed = tryingToStop ? (targetSpeed + speed) / 1.5f : speed;
+        Vector3 movementVector = GetMovementUnitVector(targetMovementAngle) * finalSpeed;
         controller.SimpleMove(movementVector);
-        animator.SetFloat("VelocityZ", GetPercentageOfMaxSpeed());
+    }
+
+    private void UpdateAnimation()
+    {
+        float velocityX = Mathf.Cos(movementAngle * Mathf.Deg2Rad);
+        float velocityZ = Mathf.Sin(movementAngle * Mathf.Deg2Rad);
+
+        animator.SetFloat("VelocityZ", GetPercentageOfMaxSpeed() * velocityZ);
+        animator.SetFloat("VelocityX", GetPercentageOfMaxSpeed() * velocityX);
+    }
+
+    private void UpdateSpeed()
+    {
+        if (speed < targetSpeed)
+        {
+            speed += acceleration * Time.fixedDeltaTime;
+            speed = Mathf.Min(speed, targetSpeed);
+        }
+        else if (speed > targetSpeed)
+        {
+            speed -= decceleration * Time.fixedDeltaTime;
+            speed = Mathf.Max(speed, targetSpeed);
+        }
+
+    }
+
+    private void UpdateMovementAngle()
+    {
+        bool tryingToStop = targetSpeed == 0f;
+        float acceleration = tryingToStop ? angularDeccelleration: angularAcceleration;
+
+        print(acceleration);
+        if (movementAngle < targetMovementAngle)
+        {
+            movementAngle += acceleration * Time.fixedDeltaTime;
+            movementAngle = Mathf.Min(movementAngle, targetMovementAngle);
+        }
+        else if (movementAngle > targetMovementAngle)
+        {
+            movementAngle -= acceleration * Time.fixedDeltaTime;
+            movementAngle = Mathf.Max(movementAngle, targetMovementAngle);
+        }
+
     }
 
     private float GetPercentageOfMaxSpeed()
